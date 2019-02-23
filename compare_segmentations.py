495411d6bd8713762e1f91b2ecc61b132df8ec05
wukm <lwukmer@gmail.com>
2019-02-23 15:27:32 -0800

    segmentation demo fixed

--- compare_segmentations.py
+++ compare_segmentations.py
@@ -18,6 +18,7 @@ for a fixed gamma, beta, and range of sigma
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from placenta import (get_named_placenta, list_by_quality, cropped_args,
                       mimg_as_float, open_typefile, open_tracefile,
                       strip_ncs_name)
@@ -67,9 +68,10 @@ def split_signed_frangi_stack(F, negative_range=None, positive_range=None,
 
     return f, nf
 
-placentas = list_by_quality(0, N=1)
 
-OUTPUT_DIR = 'output/190220-segmentation_demo'
+placentas = list_by_quality(0)
+
+OUTPUT_DIR = 'output/190223-segmentation_demo'
 
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
@@ -78,6 +80,7 @@ beta =0.15
 gamma = 1.0
 N_scales = 20
 THRESHOLD = .3  # \alpha^{(+)}
+THRESHOLD_LOW = .2  # \alpha^{(+)}
 MARGIN_THRESHOLD = 0.01  # \alpha^{(-)}
 NEGATIVE_RANGE = (0,6)
 log_range = (-1.5, 3.2)
@@ -113,8 +116,13 @@ for filename in placentas:
                                     signed_frangi=True).filled(0)
                                     for sigma in scales])
 
+    Fpos = np.clip(F, 0, None) # replace negative values with 0
+    Fneg = np.clip(F, None, 0) # replace negative values with 0
+
+    # makes Vmaxpos and Vmaxneg
     f, nf = split_signed_frangi_stack(F, positive_range=None,
-                                      negative_range=(1,12), mask=bigmask)
+                                      negative_range=NEGATIVE_RANGE,
+                                      mask=bigmask)
 
     # just in case Vmax(+) and Vmax(-) are both nonzero at the same pixel, we
     # will prioritize Vmax if it is above THRESHOLD, otherwise Vmin if it is
@@ -122,39 +130,32 @@ for filename in placentas:
     # not sure what i really want to show here, maybe just Vmax(+) or just a
     # each over thresholds (i.e. a 3 color map)
 
-    recolor_with_negative = (f <= THRESHOLD) & (nf > MARGIN_THRESHOLD)
-
-    fboth = f.copy()
-    fboth[recolor_with_negative] = nf[recolor_with_negative]
 
     spine = dilate_boundary(f, mask=img.mask, radius=20).filled(0)
-    spine = rescale_intensity(spine, in_range=(0,1), out_range='uint8')
-    ecp_spine = spine.astype('uint8')
-    ecp_spine = ecp(ecp_spine, disk(3)) > (THRESHOLD*255)
 
+
+    spineseeds = (spine > THRESHOLD)
     margins = (nf > MARGIN_THRESHOLD)
 
-    # trough filling with ECP prefilter
-    approx_td_ecp, radii = dilate_to_rim(ecp_spine, margins, thin_spine=False, return_radii=True)
 
-    approx2, radii2 = dilate_to_rim(spine > THRESHOLD, margins, return_radii=True)
+    approx_tf, radii = dilate_to_rim(spineseeds > THRESHOLD, margins,
+                                    return_radii=True)
 
     # fixed threshold
-    approx_FA = (spine > THRESHOLD)
+    approx_FA_high = (spine > THRESHOLD)
+    approx_FA_low = (spine > THRESHOLD_LOW)
 
-    # fixed threshold after ECP prefilter
-    approx_PFA = ecp_spine
 
     # find alphas of each scale (NOTE: THIS DOES *NOT* omit the last 4 scales)
     # scalewise for 95th percentile
-    ALPHAS = np.array([nz_percentile(F[k], 95.0)
+    ALPHAS_95 = np.array([nz_percentile(Fpos[k], 95.0)
                        for k in range(len(scales))])
-    ALPHAS_98 = np.array([nz_percentile(F[k], 98.0)
+    ALPHAS_98 = np.array([nz_percentile(Fpos[k], 98.0)
                        for k in range(len(scales))])
 
-    approx_PF = apply_threshold(np.transpose(F,(1,2,0)), ALPHAS,
+    approx_PF95 = apply_threshold(np.transpose(Fpos,(1,2,0)), ALPHAS_95,
                                 return_labels=False)
-    approx_PF98 = apply_threshold(np.transpose(F,(1,2,0)), ALPHAS_98,
+    approx_PF98 = apply_threshold(np.transpose(Fpos,(1,2,0)), ALPHAS_98,
                                 return_labels=False)
 
     precision_score = lambda t: int(t[0]) / int(t[0] + t[2])
@@ -163,86 +164,93 @@ for filename in placentas:
     m_st, counts_st = mcc(straw, trace, bg_mask=img.mask, return_counts=True)
     p_st = precision_score(counts_st)
 
-    # mcc, counts, precision f
-    #trough_fillings with ECP prefilter
-    m_td_ecp, counts_td_ecp = mcc(approx_td_ecp, trace, bg_mask=img.mask, return_counts=True)
-    p = precision_score(counts)
-
-    # mcc, counts, precision for trough_fillings w/o ECP prefilter
-    m2, counts2 = mcc(approx2, trace, bg_mask=img.mask, return_counts=True)
-    p2 = precision_score(counts2)
-
-    # mcc, counts, precision for fixed threshold with ECP prefiler
-    m_PFA, counts_PFA = mcc(approx_PFA, trace, bg_mask=img.mask,
-                            return_counts=True)
-    p_PFA = precision_score(counts_PFA)
-
     # mcc, counts, precision for fixed threshold w/o prefilter
-    m_FA, counts_FA = mcc(approx_FA, trace, bg_mask=img.mask,
+    m_FA_low, counts_FA_low = mcc(approx_FA_low, trace, bg_mask=img.mask,
                             return_counts=True)
-    p_FA = precision_score(counts_FA)
+    p_FA_low = precision_score(counts_FA_low)
+
+    m_FA_high, counts_FA_high = mcc(approx_FA_high, trace, bg_mask=img.mask,
+                            return_counts=True)
+    p_FA_high = precision_score(counts_FA_high)
 
     # mcc, counts, precision for scalewise percent filtelr
-    m_PF, counts_PF = mcc(approx_PF, trace, bg_mask=img.mask,
+    m_PF95, counts_PF95 = mcc(approx_PF95, trace, bg_mask=img.mask,
                           return_counts=True)
-    p_PF = precision_score(counts_PF)
+    p_PF95 = precision_score(counts_PF95)
 
     m_PF98, counts_PF98 = mcc(approx_PF98, trace, bg_mask=img.mask,
                           return_counts=True)
-    p_PF98 = precision_score(counts_PF)
+    p_PF98 = precision_score(counts_PF98)
 
-    fig, ax = plt.subplots(nrows=2, ncols=4, figsize=(25,15))
+    #mcc, counts, precision for trough_fillings w/o ECP prefilter
+    m_tf, counts_tf = mcc(approx_tf, trace, bg_mask=img.mask, return_counts=True)
+    p_tf = precision_score(counts_tf)
+
+
+
+
+    # this is just used for visualization, not very meaningful though
+    #recolor_with_negative = (f <= THRESHOLD) & (nf > MARGIN_THRESHOLD)
+    #fboth = f.copy()
+    #fboth[recolor_with_negative] = nf[recolor_with_negative]
+
+    sns.set(font_scale=0.8)
+    fig, ax = plt.subplots(nrows=2, ncols=4, figsize=(13,7))
 
     ax[0,0].imshow(img[crop], cmap=plt.cm.gray)
     ax[0,0].set_title(basename)
 
-    ax[1,0].imshow(confusion(straw,trace)[crop])
-    ax[1,0].set_title(rf'   ISODATA threshold (Frangi-less)', loc='left')
-    ax[1,0].set_title(f'MCC: {m_st:.2f}\n'
-                        f'precision: {p_st:.2%}', loc='right')
 
-    ax[0,1].imshow(confusion(approx_FA, trace)[crop])
+    ax[0,1].imshow(confusion(approx_FA_high, trace)[crop])
     ax[0,1].set_title(rf'   fixed $\alpha={THRESHOLD}$', loc='left')
-    ax[0,1].set_title(f'MCC: {m_FA:.2f}\n'
-                        f'precision: {p_FA:.2%}', loc='right')
+    ax[0,1].set_title(f'MCC: {m_FA_high:.2f}\n'
+                        f'precision: {p_FA_high:.2%}', loc='right')
 
+    ax[0,2].imshow(confusion(approx_FA_low, trace)[crop])
+    ax[0,2].set_title(rf'   fixed $\alpha={THRESHOLD_LOW}$', loc='left')
+    ax[0,2].set_title(f'MCC: {m_FA_low:.2f}\n'
+                        f'precision: {p_FA_low:.2%}', loc='right')
     #ax[1,1].imshow(confusion(bspine,trace)[crop])
     #ax[1,1].set_title(rf'   local percentile $\alpha={THRESHOLD}$', loc='left')
     #ax[1,1].set_title(f'MCC: {m_PFA:.2f}\n'
     #                    f'precision: {p_PFA:.2%}', loc='right')
 
-    ax[1,1].imshow(confusion(approx_PF,trace)[crop])
-    ax[1,1].set_title(rf'   nz-percentile threshold (p=95)', loc='left')
-    ax[1,1].set_title(f'MCC: {m_PF:.2f}\n'
-                        f'precision: {p_PF:.2%}', loc='right')
+    ax[1,1].imshow(confusion(approx_PF95,trace)[crop])
+    ax[1,1].set_title(f'   scalewise nz-percentile\n   (p=95)', loc='left')
+    ax[1,1].set_title(f'MCC: {m_PF95:.2f}\n'
+                        f'precision: {p_PF95:.2%}', loc='right')
 
-    ax[0,2].imshow(confusion(approx2,trace)[crop])
-    ax[0,2].set_title(rf'   trough filling', loc='left')
-    ax[0,2].set_title(f'MCC: {m2:.2f}\n'
-                        f'precision: {p2:.2%}', loc='right')
+    ax[1,2].imshow(confusion(approx_PF98,trace)[crop])
+    ax[1,2].set_title(f'   scalewise nz-percentile\n   (p=98)', loc='left')
+    ax[1,2].set_title(f'MCC: {m_PF98:.2f}\n'
+                        f'precision: {p_PF98:.2%}', loc='right')
+    ax[1,3].imshow(confusion(approx_tf,trace)[crop])
+    ax[1,3].set_title(rf'   trough filling', loc='left')
+    ax[1,3].set_title(f'MCC: {m_tf:.2f}\n'
+                        f'precision: {p_tf:.2%}', loc='right')
 
-    ax[1,2].imshow(confusion(approx,trace)[crop])
-    ax[1,2].set_title(rf'   trough filling w/ ECP prefilter', loc='left')
-    ax[1,2].set_title(f'MCC: {m:.2f}\n'
-                        f'precision: {p:.2%}', loc='right')
+    ax[0,3].imshow(confusion(straw,trace)[crop])
+    ax[0,3].set_title(f'   ISODATA threshold\n   (Frangi-less)', loc='left')
+    ax[0,3].set_title(f'MCC: {m_st:.2f}\n'
+                        f'precision: {p_st:.2%}', loc='right')
 
-    ax[0,3].imshow(fboth[crop], vmin=-1.0, vmax=1.0, cmap='seismic')
-    ax[0,3].set_title(rf'V_max (signed), $\beta={beta}, \gamma={gamma}$')
+    ax[1,0].imshow(f[crop], cmap='nipy_spectral', vmax=1.0, vmin=0.0)
+    ax[1,0].set_title(rf'V_max $\beta={beta}, \gamma={gamma}$')
 
-    ax[1,3].imshow(trace[crop])
     #fig.subplots_adjust(right=0.9, wspace=0.05, hspace=0.1)
     #cbar_ax = fig.add_axes([0.9, 0.15, 0.05, 0.7])
     #fig.colorbar(im, cax=cbar_ax, shrink=0.5)
 
     [a.axis('off') for a in ax.ravel()]
-    fig.subplots_adjust(wspace=0.05, hspace=0.1)
+    #fig.subplots_adjust(wspace=0.05, hspace=0.1)
+    fig.tight_layout()
     plt.savefig(os.path.join(OUTPUT_DIR, ''.join(('fig-', basename, '.png'))))
     plt.show()
 
-    mccs.append((filename, m_FA, m_PF, m_PFA, m, m2, m_st))
-    precs.append((filename, p_FA, p_PF, p_PFA, p, p2, p_st))
+    mccs.append((filename, m_FA_high, m_FA_low, m_PF95, m_PF98, m_st))
+    precs.append((filename, p_FA_high, p_FA_low, p_PF95, p_PF98, p_st))
 
 runlog = { 'mccs': mccs, 'precs': precs}
 
-#with open(os.path.join(OUTPUT_DIR,'runlog.json'), 'w') as f:
-#    json.dump(runlog, f, indent=True)
+with open(os.path.join(OUTPUT_DIR,'runlog.json'), 'w') as f:
+    json.dump(runlog, f, indent=True)
